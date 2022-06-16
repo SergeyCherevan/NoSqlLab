2c6d2cc5129f80df3d81089451a05442c94d17a5
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2022-06-16 05:31:27 +0300

    add: footer html

--- NoSqlLab/ClientApp/src/app/components/footer/footer.component.ts
+++ NoSqlLab/ClientApp/src/app/components/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
 })
 export class FooterComponent implements OnInit {
+
+  @Input() title: string = "";
+
+  sectionStr: string = "Секции";
+  homeStr: string = "Домашняя";
+  usersStr: string = "Пользователи";
+
+  get copyrightStr(): string {
+    return `© 2022 ${this.title}, Inc. All rights reserved.`;
+  }
 
   constructor() { }
 
